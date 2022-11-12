9fd55824aa9ff420cafbe12f732c3f4d2e903168
Aroon Salotra <aroonsalotra@gmail.com>
2022-11-12 23:45:52 +0000

    add mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,15 @@
 import Link from "next/link"
+import { GiHamburgerMenu } from "react-icons/gi"
+import { useState } from "react"
 
 const Navbar = () => {
+
+    const [mobileNav, setMobileNav] = useState(false)
+
+    const handleClick = () => {
+        return setMobileNav(state => !mobileNav)
+    }
+
 
     const navbarList: { title: string, linkTo: string }[] = [
         { title: "Home", linkTo: "/" },
@@ -10,17 +19,27 @@ const Navbar = () => {
 
     return (
         <nav className="navbar bg-col-a">
-            {navbarList.map(({ title, linkTo }) => {
-                return <Link key={title}
-                    href={linkTo}>
-                    {title}
-                </Link>
-            })}
-            <a href="https://github.com/AroonSalotra"
-                target={"_blank"}
-                rel="noreferrer">
-                My Github
-            </a>
+
+            <GiHamburgerMenu
+                className={"navbar-mobile"}
+                onClick={handleClick}
+            />
+
+            <div className={`navbar-desktop ${mobileNav ? "show" : ""}`}>
+                {navbarList.map(({ title, linkTo }) => {
+                    return <Link key={title}
+                        href={linkTo}>
+                        {title}
+                    </Link>
+                })}
+
+                <a href="https://github.com/AroonSalotra"
+                    target={"_blank"}
+                    rel="noreferrer">
+                    My Github
+                </a>
+            </div>
+
         </nav>
     );
 }
